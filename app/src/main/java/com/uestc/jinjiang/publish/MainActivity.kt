c9bdfefed696c8819535381c803665c14a2cccf6
feifeiPeng <pff272316@alibaba-inc.com>
2021-06-05 11:11:39 +0800

    [新增]----tabUI

--- app/src/main/java/com/uestc/jinjiang/publish/MainActivity.kt
+++ app/src/main/java/com/uestc/jinjiang/publish/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.uestc.jinjiang.publish
 
-import android.Manifest
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.uestc.jinjiang.publish.databinding.ActivityMainBinding
-import com.uestc.jinjiang.publish.file.ppt.PptActivity
 import com.uestc.run.basebase.BaseActivity
 
 
@@ -22,7 +16,6 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
-        missionPermission()
     }
 
 
@@ -32,61 +25,9 @@ class MainActivity : BaseActivity() {
             startActivity(Intent(this, TabActivity::class.java))
         }
         binding.item02.setOnClickListener {
-//            startActivity(Intent(this, MainFileActivity::class.java))
-            missionPermission()
-            chooseFile(this@MainActivity, 1002)
         }
 
     }
 
-    fun chooseFile(activity: Activity?, requestCode: Int) {
-        val mimeTypes = arrayOf(
-            MimeType.PDF,
-            MimeType.PPT,
-            MimeType.VIDEO,
-        )
-
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-        intent.type = "*/*"
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        activity?.startActivityForResult(intent, requestCode)
-    }
-
-    private fun missionPermission() {
-        if (ContextCompat.checkSelfPermission(
-                applicationContext,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this@MainActivity,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-            ) {
-            } else {
-                ActivityCompat.requestPermissions(
-                    this@MainActivity,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    PptActivity.REQUEST_PERMISSION
-                )
-            }
-        }
-    }
-
-
-
 
 }
-
-object MimeType {
-    const val DOC = "application/msword"
-    const val DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    const val XLS = "application/vnd.ms-excel application/x-excel"
-    const val XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    const val PPT = "application/vnd.ms-powerpoint"
-    const val PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    const val PDF = "application/pdf"
-    const val VIDEO = "video/*"
-}
-
---
+++ app/src/main/java/com/uestc/jinjiang/publish/extend/FileSelect.kt
@@ -0,0 +1,87 @@
+package com.uestc.jinjiang.publish.extend
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.net.Uri
+import com.uestc.jinjiang.publish.R
+import droidninja.filepicker.FilePickerBuilder
+import droidninja.filepicker.FilePickerConst.PERMISSIONS_FILE_PICKER
+import droidninja.filepicker.FilePickerConst.SPAN_TYPE
+import droidninja.filepicker.models.sort.SortingTypes
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import java.util.*
+
+const val RC_PHOTO_PICKER_PERM = 123
+const val RC_FILE_PICKER_PERM = 321
+private const val CUSTOM_REQUEST_CODE = 532
+private const val MAX_ATTACHMENT_COUNT = 10
+
+
+@AfterPermissionGranted(RC_FILE_PICKER_PERM)
+fun Activity.picDoc() {
+    if (EasyPermissions.hasPermissions(this, PERMISSIONS_FILE_PICKER)) {
+        onPickDoc()
+    } else {
+        EasyPermissions.requestPermissions(
+            this, getString(R.string.rationale_doc_picker),
+            RC_FILE_PICKER_PERM, PERMISSIONS_FILE_PICKER
+        )
+    }
+}
+
+@AfterPermissionGranted(RC_PHOTO_PICKER_PERM)
+fun Activity.picImage() {
+    if (EasyPermissions.hasPermissions(this, PERMISSIONS_FILE_PICKER)) {
+        onPickPhoto()
+    } else {
+        EasyPermissions.requestPermissions(
+            this,
+            getString(R.string.rationale_photo_picker),
+            RC_PHOTO_PICKER_PERM,
+            PERMISSIONS_FILE_PICKER
+        )
+    }
+}
+
+fun Activity.onPickDoc(): ArrayList<Uri> {
+    val docPaths = ArrayList<Uri>()
+    FilePickerBuilder.instance
+        .setMaxCount(1)
+        .setSelectedFiles(docPaths)
+        .setActivityTheme(R.style.FilePickerTheme)
+        .setActivityTitle("请选择文件")
+        .setImageSizeLimit(50)
+        .setVideoSizeLimit(200)
+        .enableDocSupport(true)
+        .enableSelectAll(false)
+        .sortDocumentsBy(SortingTypes.NAME)
+        .withOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+        .pickFile(this)
+    return docPaths
+}
+
+fun Activity.onPickPhoto(): ArrayList<Uri> {
+    val photoPaths = ArrayList<Uri>()
+    FilePickerBuilder.instance
+        .setMaxCount(10)
+        .setSelectedFiles(photoPaths)
+        .setActivityTheme(R.style.FilePickerTheme)
+        .setActivityTitle("请选择图片或视频")
+        .setImageSizeLimit(50)
+        .setVideoSizeLimit(200)
+        .setSpan(SPAN_TYPE.FOLDER_SPAN, 3)
+        .setSpan(SPAN_TYPE.DETAIL_SPAN, 4)
+        .enableVideoPicker(true)
+        .enableCameraSupport(true)
+        .showGifs(true)
+        .showFolderView(false)
+        .enableSelectAll(true)
+        .enableImagePicker(true)
+        .setCameraPlaceholder(R.drawable.custom_camera)
+        .withOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+        .pickPhoto(this, CUSTOM_REQUEST_CODE)
+    return photoPaths;
+
+}
+
